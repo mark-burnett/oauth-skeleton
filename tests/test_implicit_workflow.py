0ba67e4f45a1c8543f93dcff868494feeb940a29
Mark Burnett <mark.m.burnett@gmail.com>
2014-07-14 08:15:44 -0500

    add test that gets resource with existing credentials

--- tests/test_implicit_workflow.py
+++ tests/test_implicit_workflow.py
@@ -14,6 +14,18 @@ class TestWebWorkflow(unittest.TestCase):
 
         self.assertEqual(expected_resource, actual_resource)
 
+    def test_client_gets_resource_twice(self):
+        c = ClientSDK('alice:k')
+        first_resource = c.get_direct_resource('buz')
+        second_resource = c.get_direct_resource('buz')
+
+        expected_resource = {
+            'name': 'buz',
+            'data': 'some data that lives on the client',
+        }
+
+        self.assertEqual(expected_resource, second_resource)
+
 
 if __name__ == '__main__':
     unittest.main()
