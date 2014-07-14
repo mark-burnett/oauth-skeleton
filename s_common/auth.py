b44af4fe3de77b6b27134430ca8bb890eb8297dd
Mark Burnett <mark.m.burnett@gmail.com>
2014-07-14 08:13:36 -0500

    remove unused code

--- s_common/auth.py
+++ s_common/auth.py
@@ -2,12 +2,5 @@ import os
 import urllib
 
 
-def auth_url(path, **query_args):
-    base_url = os.path.join(os.environ['AUTH_URL'], path)
-
-    if query_args:
-        query_string = urllib.urlencode(query_args)
-        return '%s?%s' % (base_url, query_string)
-
-    else:
-        return base_url
+def auth_url(path):
+    return os.path.join(os.environ['AUTH_URL'], path)
